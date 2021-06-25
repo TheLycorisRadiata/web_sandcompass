da420d3d20969677f3d5a1b5ba0ce7174fc95c56
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2021-06-25 13:47:24 +0200

    Front/Back: 404 page done

--- back/app.js
+++ back/app.js
@@ -55,7 +55,7 @@ app.post('/', (req, res) =>
 
 app.get('/*', (req, res) => 
 {
-	res.status(404).json({ title: 'Page Not Found', message: 'I\'m sorry, this page doesn\'t exist.' });
+	res.status(404).json({ status: 404, title: 'Page Not Found', message: 'I\'m sorry, this page doesn\'t exist.' });
 });
 
 module.exports = app;
--- front/src/scripts/PageNotFound.js
+++ front/src/scripts/PageNotFound.js
@@ -1,9 +1,25 @@
+import {useState, useEffect} from 'react';
+
 const PageNotFound = () =>
 {
+	const [back_title, set_back_title] = useState('');
+	const [back_message, set_back_message] = useState('');
+
+	useEffect(() => 
+	{
+		fetch('http://localhost:3001/*')
+		.then(res => res.json())
+		.then(json => 
+		{
+			set_back_title(json.title);
+			set_back_message(json.message);
+		});
+	}, []);
+
 	return (
 		<main>
-			<h1>Page Not Found</h1>
-			<p id="error_message">I'm sorry, this page doesn't exist.</p>
+			<h1>{back_title}</h1>
+			<p id="error_message">{back_message}</p>
 		</main>
 	);
 }
